807498a89c94942f415804fc9ad3a781c3d222e2
Elodie S <elodie.sevestre86@gmail.com>
2026-03-27 12:23:14 +0100

    12-analyse_text.test init

--- src/12-analyze_text.test.js
+++ src/12-analyze_text.test.js
@@ -1,1 +1,9 @@
 // TODO : test analyze_text.js
+import { describe, test, expect } from "vitest";
+import { analyze_text } from "./12-analyze_text";
+
+describe("analyse_text", () => {
+  test("", () => {
+    expect(analyze_text()).toBe();
+  });
+});
